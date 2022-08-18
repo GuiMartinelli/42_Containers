a3bdc97912809508c4508e5549dd91327abefea3
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-08-17 21:08:39 -0300

    Map data using rbtree

--- Map.hpp
+++ Map.hpp
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 20:47:11 by guferrei          #+#    #+#             */
-/*   Updated: 2022/07/21 21:30:24 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/08/17 21:08:07 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,19 @@
 #include <iostream>
 #include <memory>
 #include <stdexcept>
+#include "Less.hpp"
 #include "Pair.hpp"
+#include "RBTree.hpp"
 #include "Iterator.hpp"
 #include "Reverse_iterator.hpp"
-
-
 
 namespace ft
 {
 	template< typename _Key,
 			typename _Tp,
-			typename _Compare,
+			typename _Compare = ft::less<_Key>,
 			typename _Alloc = std::allocator< ft::pair<const _Key, _Tp > > >
 	class map {
-
-		struct Node
-		{
-			ft::pair<const _Key, _Tp >	data;
-			Node						*rChild;
-			Node						*lChild;
-		};
-
-		private:
-			Node	*root;
-			size_t	size;
-			_Alloc	alloc;
-
 		public:
 			typedef _Key						key_type;
 			typedef _Tp							mapped_type;
@@ -71,21 +58,76 @@ namespace ft
 			
 					value_compare() {};
 
-					result_type	operator()(const first_argument_type& lhs, const second_argument_type& rhs) const; {
+					result_type	operator()(const first_argument_type& lhs, const second_argument_type& rhs) const {
 						return comp(lhs.first, rhs.first);
 					}
 			};
 			
 			//Member Functions
 
+			map() {
+				this->_root = this->_alloc.allocate(1);
+				this->_size = 0;
+			}
+
+			~map() {
+			}
+
 			//Element Access
+
+			mapped_type&	operator[] (const key_type& k) {
+				Node*	aux;
+
+				aux = this->_root;
+				while(aux->data) {
+					if (value_compare(aux->data.first, k))
+						aux = aux->lChild;
+					else if (value_compare(k, aux->data.first))
+						aux = aux->rChild;
+					else
+						return (&aux->data.second);
+				}
+				aux = new Node(k);
+				this->size++;
+				return (&aux->data.second);
+			}
+
+			mapped_type&	at(const key_type& k) {
+				Node*	aux;
+
+				aux = this->_root;
+				while(aux->data) {
+					if (value_compare(aux->data.first, k))
+						aux = aux->lChild;
+					else if (value_compare(k, aux->data.first))
+						aux = aux->rChild;
+					else
+						return (&aux->data.second);
+				}
+				throw std::exception();
+			}
+
+			mapped_type&	at(const key_type& k) const {
+				Node*	aux;
+
+				aux = this->_root;
+				while(aux->data) {
+					if (value_compare(aux->data.first, k))
+						aux = aux->lChild;
+					else if (value_compare(k, aux->data.first))
+						aux = aux->rChild;
+					else
+						return (&aux->data.second);
+				}
+				throw std::exception();
+			}
 
 			//Iterators
 
 			//Capacity
 
 			bool	empty() const {
-				if (this->root)
+				if (this->_root)
 					return false;
 				return true;
 			}
@@ -95,7 +137,7 @@ namespace ft
 			}
 
 			size_type	max_size() const {
-				return alloc.max_size();
+				return _alloc.max_size();
 			}
 
 			//Modifers
@@ -103,6 +145,12 @@ namespace ft
 			//Lookup
 
 			//Observers
+		private:
+			RBTree<value_type>	*_root;
+			size_t				_size;
+			_Alloc				_alloc;
+			_Compare			comp;
+
 	};
 }
 
