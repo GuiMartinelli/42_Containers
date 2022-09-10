eae43d3c92c1b8b35600414985e55d0b6b7632dc
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-09-10 15:30:13 -0300

    files renamed

---
+++ reverse_iterator.hpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_iterator.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 21:19:10 by guferrei          #+#    #+#             */
+/*   Updated: 2022/09/10 15:28:55 by guferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REVERSE_ITERATOR_HPP
+#define REVERSE_ITERATOR_HPP
+
+#include "iterator.hpp"
+
+namespace ft {
+	template <typename T>
+	class	reverse_iterator : public iterator<T> {
+		public:
+			reverse_iterator() {};
+
+			reverse_iterator(T *ptr) {
+				this->it = ptr;
+			};
+
+			reverse_iterator(reverse_iterator const & obj) {
+				*this = obj;
+			};
+			
+			~reverse_iterator() {};
+
+			bool	operator<(reverse_iterator const & obj) {
+				return !(this->it < obj.it);
+			}
+
+			bool	operator>(reverse_iterator const & obj) {
+				return !(this->it > obj.it);
+			}
+
+			bool	operator<=(reverse_iterator const & obj) {
+				return !(this->it <= obj.it);
+			}
+
+			bool	operator>=(reverse_iterator const & obj) {
+				return !(this->it >= obj.it);
+			}
+
+			reverse_iterator	operator++() {
+				this->it--;
+
+				return *this;
+			}
+
+			reverse_iterator	operator--() {
+				this->it++;
+
+				return *this;
+			}
+
+			reverse_iterator	operator++(T) {
+				reverse_iterator	tmp(*this);
+				this->it--;
+
+				return tmp;
+			}
+
+			reverse_iterator	operator--(T) {
+				reverse_iterator	tmp(*this);
+				this->it++;
+
+				return tmp;
+			}
+		};
+}
+
+#endif
