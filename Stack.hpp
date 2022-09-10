11a7473055ab9b337fe28a1c1d784d8d4f42de09
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-09-10 12:52:15 -0300

    stack finished

--- Stack.hpp
+++ Stack.hpp
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 12:18:40 by guferrei          #+#    #+#             */
-/*   Updated: 2022/09/10 12:34:51 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/09/10 12:51:55 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 
 namespace ft {
 
-	template < class T, class Container = ft::vector<T>>
-	class Stack
+	template < class T, class Container = ft::vector<T> >
+	class stack
 	{
 	protected:
 		Container	_data;
@@ -27,41 +27,49 @@ namespace ft {
 		//Member Type
 
 		typedef Container					container_type;
-		typedef Container::value_type		value_type;
-		typedef Container::size_type		size_type;
-		typedef Container::reference		reference;
-		typedef Container::const_reference	const_reference;
+		typedef typename Container::value_type		value_type;
+		typedef typename Container::size_type		size_type;
+		typedef typename Container::reference		reference;
+		typedef typename Container::const_reference	const_reference;
 
 		//Member Functions
 
-		Stack() {};
-		~Stack() {};
-		Stack&	operator=(Stack& obj) {};
+		stack() {};
+
+		~stack() {};
+
+		stack&	operator=(stack& obj) {
+			this->_data = obj._data;
+		};
 
 		//Element Access
 
-		reference	top() {};
+		reference	top() {
+			return this->_data.back();
+		}
 
-		const_reference	top() const {};
+		const_reference	top() const {
+			return this->_data.back();
+		}
 
 		//Capacity
 
 		bool	empty() const {
-			return this->_data->empty();
+			return this->_data.empty();
 		}
 
 		size_type	size() const {
-			return this->_data->size();
+			return this->_data.size();
 		}
 
 		//Modifiers
 
 		void	push( const value_type& value) {
-			
+			this->_data.push_back(value);
 		}
 
 		void	pop() {
-			
+			this->_data.pop_back();
 		}
 	};
 }
