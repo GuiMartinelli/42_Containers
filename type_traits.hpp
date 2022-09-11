14a01ca6c1e713c0bf49015d7c618b0a70bc8ed7
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-09-11 14:53:17 -0300

    all types checked is_integral

--- type_traits.hpp
+++ type_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 13:51:39 by guferrei          #+#    #+#             */
-/*   Updated: 2022/09/11 14:24:34 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/09/11 14:51:48 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,73 @@ namespace ft
 	typedef integral_constant<bool, false>	false_type;
 	typedef integral_constant<bool, true>	true_type;
 
-
 	template< typename T >
 	struct is_integral {
 		typedef false_type	type;
 	};
 
 	template<>
+	struct is_integral<bool> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<char> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<wchar_t> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<signed char> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<short int> {
+		typedef true_type	type;
+	};
+
+	template<>
 	struct is_integral<int> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<long int> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<long long int> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<unsigned char> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<unsigned short int> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<unsigned int> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<unsigned long int> {
+		typedef true_type	type;
+	};
+
+	template<>
+	struct is_integral<unsigned long long int> {
 		typedef true_type	type;
 	};
 }
