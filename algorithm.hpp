55bcab116c01e011b8050a1fbfbaacce222548b1
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-09-11 13:06:07 -0300

    ft::lexicographical_compare implemented

--- algorithm.hpp
+++ algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 12:29:46 by guferrei          #+#    #+#             */
-/*   Updated: 2022/09/11 12:51:10 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/09/11 13:00:57 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ namespace ft {
 	}
 
 	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
-	bool	equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred) {
+	bool	equal(InputIterator1 first1, InputIterator1 last1,
+					InputIterator2 first2, BinaryPredicate pred) {
 		while (first1 != last1) {
 			if (!pred(*first1, *first2))
 				return false;
@@ -35,6 +36,35 @@ namespace ft {
 			++first2;
 		}
 		return true;
+	}
+
+	template <class InputIterator1, class InputIterator2>
+	bool	lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+									InputIterator2 first2, InputIterator2 last2) {
+		while (first1 != last1) {
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
+
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool	lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+									InputIterator2 first2, InputIterator2 last2,
+									Compare comp) {
+		while (first1 != last1) {
+			if (first2 == last2 || comp(*first2, *first1))
+				return false;
+			else if (comp(*first1, *first2))
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
 	}
 }
 
