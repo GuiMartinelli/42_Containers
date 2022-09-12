a23664737ab702138994781ec28b646ed28c95ed
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-09-12 11:13:21 -0300

    is_integral value working

--- type_traits.hpp
+++ type_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 13:51:39 by guferrei          #+#    #+#             */
-/*   Updated: 2022/09/11 14:58:05 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/09/12 11:12:56 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,80 +23,55 @@ namespace ft
 		typedef T	type;
 	};
 
-	template< class T, T value >
-	struct integral_constant;
+	template< class T, T v >
+	struct integral_constant {
+		static const T value = v;
+	};
+
 	typedef integral_constant<bool, false>	false_type;
 	typedef integral_constant<bool, true>	true_type;
 
 	template< typename T >
-	struct is_integral {
-		typedef false_type	type;
-	};
+	struct is_integral : false_type {};
 
 	template<>
-	struct is_integral<bool> {
-		typedef true_type	type;
-	};
+	struct is_integral<bool> : true_type {};
 
 	template<>
-	struct is_integral<char> {
-		typedef true_type	type;
-	};
+	struct is_integral<char> : true_type {};
 
 	template<>
-	struct is_integral<wchar_t> {
-		typedef true_type	type;
-	};
+	struct is_integral<wchar_t> : true_type {};
 
 	template<>
-	struct is_integral<signed char> {
-		typedef true_type	type;
-	};
+	struct is_integral<signed char> : true_type {};
 
 	template<>
-	struct is_integral<short int> {
-		typedef true_type	type;
-	};
+	struct is_integral<short int> : true_type {};
 
 	template<>
-	struct is_integral<int> {
-		typedef true_type	type;
-	};
+	struct is_integral<int> : true_type {};
 
 	template<>
-	struct is_integral<long int> {
-		typedef true_type	type;
-	};
+	struct is_integral<long int> : true_type {};
 
 	template<>
-	struct is_integral<long long int> {
-		typedef true_type	type;
-	};
+	struct is_integral<long long int> : true_type {};
 
 	template<>
-	struct is_integral<unsigned char> {
-		typedef true_type	type;
-	};
+	struct is_integral<unsigned char> : true_type {};
 
 	template<>
-	struct is_integral<unsigned short int> {
-		typedef true_type	type;
-	};
+	struct is_integral<unsigned short int> : true_type {};
 
 	template<>
-	struct is_integral<unsigned int> {
-		typedef true_type	type;
-	};
+	struct is_integral<unsigned int> : true_type {};
 
 	template<>
-	struct is_integral<unsigned long int> {
-		typedef true_type	type;
-	};
+	struct is_integral<unsigned long int> : true_type {};
 
 	template<>
-	struct is_integral<unsigned long long int> {
-		typedef true_type	type;
-	};
+	struct is_integral<unsigned long long int> : true_type {};
 }
 
 
