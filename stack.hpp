0d9607f9c1e7ab3d071b2c7fcb202c6ab9944ed5
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-09-12 19:17:27 -0300

    stack swap non-member function

--- stack.hpp
+++ stack.hpp
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 12:18:40 by guferrei          #+#    #+#             */
-/*   Updated: 2022/09/10 18:46:49 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/09/12 19:16:52 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,9 @@ namespace ft {
 
 		template < class Tp, class Cont >
 		friend bool	operator>=(stack< Tp, Cont > const & lhs, stack< Tp, Cont > const & rhs);
+
+		template < class Tp, class Cont >
+		friend void	swap(stack< Tp, Cont > & lhs, stack< Tp, Cont > & rhs);
 
 	public:
 		//Member Type
@@ -122,6 +125,29 @@ namespace ft {
 	template < class T, class Container = ft::vector<T> >
 	bool	operator>=(stack< T, Container > const & lhs, stack< T, Container > const & rhs) {
 		return rhs._data >= lhs._data;
+	}
+
+	template < class T, class Container = ft::vector<T> >
+	void	swap(stack< T, Container > & lhs, stack< T, Container > & rhs) {
+		stack< T, Container >	aux1;
+		stack< T, Container >	aux2;
+
+		while (lhs.size() > 0) {
+			aux1.push(lhs.top());
+			lhs.pop();
+		}
+		while (rhs.size() > 0) {
+			aux2.push(rhs.top());
+			rhs.pop();
+		}
+		while (aux2.size() > 0) {
+			lhs.push(aux2.top());
+			aux2.pop();
+		}
+		while (aux1.size() > 0) {
+			rhs.push(aux1.top());
+			aux1.pop();
+		}
 	}
 }
 
