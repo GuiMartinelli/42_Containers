929c0660234fe5d534426c74357fa3adc2291c67
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-09-19 20:28:49 -0300

    binary_function class created, include name fixed

--- functional.hpp
+++ functional.hpp
@@ -6,14 +6,21 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 20:28:05 by guferrei          #+#    #+#             */
-/*   Updated: 2022/09/11 13:24:42 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/09/19 20:22:52 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LESS_HPP
-#define LESS_HPP
+#ifndef FUNCTIONAL_HPP
+#define FUNCTIONAL_HPP
 
 namespace ft {
+	template <class Arg1, class Arg2, class Result>
+	struct binary_fuction {
+		typedef Arg1	first_argument_type;
+		typedef Arg2	second_argument_type;
+		typedef Result	result_type;
+	};
+
 	template<class T>
 	struct less {
 		bool	operator()(T const & x, T const & y) const {
