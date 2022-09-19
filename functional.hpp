722f4ead6062cb8e6ab5195c12539c2ac399cad2
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-09-19 20:50:06 -0300

    binary_function struct name fix

--- functional.hpp
+++ functional.hpp
@@ -6,7 +6,7 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 20:28:05 by guferrei          #+#    #+#             */
-/*   Updated: 2022/09/19 20:22:52 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/09/19 20:49:14 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 namespace ft {
 	template <class Arg1, class Arg2, class Result>
-	struct binary_fuction {
+	struct binary_function {
 		typedef Arg1	first_argument_type;
 		typedef Arg2	second_argument_type;
 		typedef Result	result_type;
