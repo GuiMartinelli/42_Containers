3fc7144478e8b7c0953eb1277883d8a687489739
Guilherme Martinelli <guilhermefmartinelli@gmail.com>
2022-10-04 19:31:07 -0300

    more stack test

--- tests/test_stack_ft.cpp
+++ tests/test_stack_ft.cpp
@@ -6,15 +6,17 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 19:06:53 by guferrei          #+#    #+#             */
-/*   Updated: 2022/10/04 19:17:38 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/10/04 19:29:35 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "../stack.hpp"
+#include <deque>
 
 int	main(void) {
 	ft::stack<char>	st;
+	ft::stack<char>	st2;
 	clock_t			start;
 	clock_t			end;
 
@@ -46,6 +48,25 @@ int	main(void) {
 	std::cout << "    4.1 Top before pop: " << st.top() << std::endl;
 	st.pop();
 	std::cout << "    4.2 Top after pop: " << st.top() << std::endl << std::endl;
+
+	std::cout << "5. Testing non-Members operators" << std::endl;
+
+	st2.push('a');
+	st2.push('b');
+
+	std::cout << "    5.1 st == st2: " << (st == st2) << std::endl;
+	std::cout << "    5.2 st != st2: " << (st != st2) << std::endl;
+	std::cout << "    5.3 st < st2: " << (st < st2) << std::endl;
+	std::cout << "    5.4 st > st2: " << (st > st2) << std::endl;
+	std::cout << "    5.5 st <= st2: " << (st <= st2) << std::endl;
+	std::cout << "    5.6 st >= st2: " << (st >= st2) << std::endl << std::endl;
+
+	std::cout << "5. Testing different inner container" << std::endl;
+
+	ft::stack<char, std::deque<char> >	st3;
+
+	st3.push('A');
+	st3.pop();
 
 	end = clock();
 
--- tests/test_stack_std.cpp
+++ tests/test_stack_std.cpp
@@ -6,15 +6,17 @@
 /*   By: guferrei <guferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 19:06:59 by guferrei          #+#    #+#             */
-/*   Updated: 2022/10/04 19:17:01 by guferrei         ###   ########.fr       */
+/*   Updated: 2022/10/04 19:28:46 by guferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stack>
+#include <deque>
 
 int	main(void) {
 	std::stack<char>	st;
+	std::stack<char>	st2;
 	clock_t				start;
 	clock_t				end;
 
@@ -46,6 +48,25 @@ int	main(void) {
 	std::cout << "    4.1 Top before pop: " << st.top() << std::endl;
 	st.pop();
 	std::cout << "    4.2 Top after pop: " << st.top() << std::endl << std::endl;
+
+	std::cout << "5. Testing non-Members operators" << std::endl;
+
+	st2.push('a');
+	st2.push('b');
+
+	std::cout << "    5.1 st == st2: " << (st == st2) << std::endl;
+	std::cout << "    5.2 st != st2: " << (st != st2) << std::endl;
+	std::cout << "    5.3 st < st2: " << (st < st2) << std::endl;
+	std::cout << "    5.4 st > st2: " << (st > st2) << std::endl;
+	std::cout << "    5.5 st <= st2: " << (st <= st2) << std::endl;
+	std::cout << "    5.6 st >= st2: " << (st >= st2) << std::endl << std::endl;
+
+	std::cout << "5. Testing different inner container" << std::endl;
+
+	std::stack<char, std::deque<char> >	st3;
+
+	st3.push('A');
+	st3.pop();
 
 	end = clock();
 
